b4d497060c78462b1d8c91ccc5a5417f49673916
testGit <testGit@qq.com>
2020-11-27 16:14:53 +0800

    local commit 1111

--- src/main/java/com/test/Apple.java
+++ src/main/java/com/test/Apple.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class Apple {
      public static void main(String[] args) {
-		System.out.println("this is an apple!");
+		System.out.println("this is an apple! 11111");
 	}
 }
